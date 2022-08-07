bc90d3d2d420ce0704deb82aa7c1c6e9db5b261a
Daiveed <jajadavidjid@gmail.com>
2022-08-07 17:53:58 +0100

    Project finished Fully responsive Js functionality added Back to top button added as a bonus

--- script.js
+++ script.js
@@ -24,3 +24,18 @@ emailButton.addEventListener("click", (e) => {
     return false;
   }
 });
+
+// BACK TO TOP BUTTON FUNCTIONALITY
+const button = document.querySelector("#backToTop");
+const scrollFunction = () => {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+};
+window.addEventListener("scroll", scrollFunction);
+button.addEventListener("click", (e) => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
